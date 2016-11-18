caa5ed9fcae4d06b8bd7c125424d2b30a5821fb7
Manuel Cagigas <manuel@wiris.com>
2016-11-18 10:53:58 +0100

    3.46.1.1018

--- lang/de/qtype_truefalsewiris.php
+++ lang/de/qtype_truefalsewiris.php
@@ -1,10 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 $string['truefalsewiris'] = 'Wahr/Falsch-Fragen – Mathematik und Naturwissenschaften';
 $string['truefalsewiris_help'] = 'Wie bei der standardmäßigen Wahr/Falsch-Frage. Allerdings können Sie verschiedene Fragetexte durch Einfügen von zufälligen Zahlen, Formeln oder grafischen Darstellungen erstellen. Sie können auch auf dynamische Weise die richtige Antwort festlegen. Die zufälligen Werte können auch für das Feedback verwendet werden.';
 $string['editingtruefalsewiris'] = 'Wahr/Falsch-Frage bearbeiten – Mathematik und Naturwissenschaften von WIRIS';
 $string['addingtruefalsewiris'] = 'Wahr/Falsch-Frage hinzufügen – Mathematik und Naturwissenschaften von WIRIS';
 $string['truefalsewirissummary'] = 'Wie bei der standardmäßigen Wahr/Falsch-Frage. Allerdings können Sie verschiedene Fragetexte durch Einfügen von zufälligen Zahlen, Formeln oder grafischen Darstellungen erstellen. Sie können auch auf dynamische Weise die richtige Antwort festlegen. Die zufälligen Werte können auch für das Feedback verwendet werden.';
-$string['truefalsewirisoverrideanswer_identifier_help'] = 'Wird die richtige Antwort festgelegt, so wird durch den entsprechenden Wert das nachstehende Feld „Richtige Antwort” übergangen. Schreiben Sie in die Registerkarte „Variablen“ einen Algorithmus, um diese Variable auf „wahr“ oder „falsch“ zu setzen.';
+$string['truefalsewirisoverrideanswer_identifier_help'] = 'Wird die richtige Antwort festgelegt, so wird durch den entsprechenden Wert das nachstehende Feld „Richtige Antwort�? übergangen. Schreiben Sie in die Registerkarte „Variablen“ einen Algorithmus, um diese Variable auf „wahr“ oder „falsch“ zu setzen.';
 $string['truefalsewirisoverrideanswer'] = '';
 $string['truefalsewirisoverrideanswer_identifier'] = 'Variable der richtigen Antwort';;
 $string['truefalsewiris_algorithm'] = 'Algorithmus';
@@ -13,10 +28,9 @@ $string['truefalsewiris_feedback_wrong_response'] = 'Feedback für die falsche A
 $string['truefalsewiris_feedback_right_response'] = 'Feedback für die richtige Antwort';
 $string['truefalsewiris_wiris_variables'] = 'WIRIS Variablen ';
 
-//From Moodle 2.3
+// From Moodle 2.3.
 $string['pluginname'] = 'Wahr/Falsch-Fragen – Mathematik und Naturwissenschaften';
 $string['pluginname_help'] = 'Wie bei der standardmäßigen Wahr/Falsch-Frage. Allerdings können Sie verschiedene Fragetexte durch Einfügen von zufälligen Zahlen, Formeln oder grafischen Darstellungen erstellen. Sie können auch auf dynamische Weise die richtige Antwort festlegen. Die zufälligen Werte können auch für das Feedback verwendet werden.';
 $string['pluginnamesummary'] = 'Wie bei der standardmäßigen Wahr/Falsch-Frage. Allerdings können Sie verschiedene Fragetexte durch Einfügen von zufälligen Zahlen, Formeln oder grafischen Darstellungen erstellen. Sie können auch auf dynamische Weise die richtige Antwort festlegen. Die zufälligen Werte können auch für das Feedback verwendet werden.';
 $string['pluginnameediting'] = 'Wahr/Falsch-Frage bearbeiten – Mathematik und Naturwissenschaften von WIRIS';
 $string['pluginnameadding'] = 'Wahr/Falsch-Frage hinzufügen – Mathematik und Naturwissenschaften von WIRIS';
-?>
